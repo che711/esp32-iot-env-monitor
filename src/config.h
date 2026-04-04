21aaf9b8575072973d2a8c35f7a0378fc2519bfc
che711 <che7111987@gmail.com>
2026-04-04 21:08:47 +0200

    update config.h for testing

--- src/config.h
+++ src/config.h
@@ -33,10 +33,10 @@ inline constexpr int BATTERY_STDBY_PIN = 3; // GPIO3 - STDBY pin от TP4056
 // ============================================
 // Timing Configuration
 // ============================================
-inline constexpr unsigned long SENSOR_INTERVAL        = 10000;  // Опрос датчика каждые 10 сек
+inline constexpr unsigned long SENSOR_INTERVAL        = 2000;  // Опрос датчика каждую 1 сек
 inline constexpr unsigned long WIFI_CHECK_INTERVAL    = 300000; // Проверка WiFi каждые 5 мин (было 50 мин!)
-inline constexpr unsigned long STATS_UPDATE_INTERVAL  = 10000;  // Обновление статистики каждые 10 сек
-inline constexpr unsigned long BATTERY_CHECK_INTERVAL = 30000;  // Проверка батареи каждые 30 сек
+inline constexpr unsigned long STATS_UPDATE_INTERVAL  = 5000;  // Обновление статистики каждые 5 сек
+inline constexpr unsigned long BATTERY_CHECK_INTERVAL = 10000;  // Проверка батареи каждые 10 сек
 
 // ============================================
 // History Configuration
